e9cf0398cb3a895d83dc2bc9a587ccff83008127
Harrison Bond <baronvonbadguy@gmail.com>
2014-08-01 13:17:19 -0400

    added new page, made some more mods

--- Gruntfile.js
+++ Gruntfile.js
@@ -136,7 +136,7 @@ module.exports = function(grunt) {
 					base: '<%= app %>/',
 					open: true,
 					livereload: true,
-					hostname: '127.0.0.1'
+					hostname: '0.0.0.0'
 				}
 			},
 			dist: {
