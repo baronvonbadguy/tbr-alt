e7540a841bf6af58b9ed5e65d13413dd29d493d4
Harrison Bond <baronvonbadguy@gmail.com>
2014-08-10 22:11:23 -0400

    fixed some javascript errors
    and cleaned up the image folder

--- app/js/app.js
+++ app/js/app.js
@@ -1,36 +1,11 @@
-var app = (function(document, $) {
-
-	'use strict';
-	var docElem = document.documentElement,
-
-		_userAgentInit = function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		},
-		_init = function() {
-			_userAgentInit();
-		};
-
-	return {
-		init: _init
-	};
-
-})(document, jQuery);
-
-(function() {
-
-	'use strict';
-	app.init();
-
-})();
-
 $(document).ready(function() {
 	'use strict';
+	document.documentElement.setAttribute('data-useragent', navigator.userAgent);
 	$(document).foundation();
 });
 
 $( window ).load(function() {
 	'use strict';
-	$('#gallery-block').attr('data-interchange', '[/images/gallery-small-hi.jpg, (small)], [/images/gallery-medium-hi.jpg, (medium)], [/images/gallery-large-hi.jpg, (large)], [/images/gallery-large-hi.jpg, (orientation: landscape)]')
+	$('#gallery-block').attr('data-interchange', '[/images/gallery-small-hi.jpg, (small)], [/images/gallery-medium-hi.jpg, (medium)], [/images/gallery-large-hi.jpg, (large)], [/images/gallery-large-hi.jpg, (orientation: landscape)]');
 	$(document).foundation('interchange', 'reflow');
-	console.log('killem');
 });
