4004174265c8e7d1b26b022de7b29d84c3a52506
Harrison Bond <baronvonbadguy@gmail.com>
2014-08-11 21:02:53 -0400

    changed gallery grid to use css background
    this way I get the fixed background benefit and it stays consistent with
    the rest of the websites look

--- app/js/app.js
+++ app/js/app.js
@@ -6,23 +6,7 @@ $(document).ready(function() {
 
 $(window).load(function() {
 	'use strict';
-  	if ($('#gallery-block').hasClass('hi-res') === false) {
-		$('#gallery-block').toggleClass('hi-res');
-		var di = $('#gallery-block').attr('data-interchange').replace(/\.jpg/g, '-hi.jpg');
-		$('#gallery-block').attr('data-interchange', di);
-		$(document).foundation();
-		$(document).foundation('interchange', 'update_images');
-		$(document).foundation('interchange', 'reflow');
+  	if ($('#page-gallery').hasClass('hi-res') === false) {
+		$('#page-gallery').toggleClass('hi-res');
 	}
-});
-
-$('#gallery-block').load( function () {
-	'use strict';
-	var src = $(this).attr('src');
-	if (src.indexOf('-hi.jpg') === -1) {
-		$(this).attr('src', src.replace(/\.jpg/g, '-hi.jpg'));
-		console.log(src);
-
-	}
-	console.log('i\'m already high you ain\'t gotta worry bout nothin');
 });
